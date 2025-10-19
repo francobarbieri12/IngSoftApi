c2786023499bbacf0ccfcd376056a9deb9fc160d
Franco Barbieri <franco.barbieri@techforb.com>
2025-10-19 03:34:44 -0300

    ADD: Initial Commit

---
+++ src/main/java/com/example/ingsoftdocker/IngSoftDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ingsoftdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngSoftDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngSoftDockerApplication.class, args);
+    }
+
+}
